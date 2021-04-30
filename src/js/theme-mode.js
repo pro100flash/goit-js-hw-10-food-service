3d743c809b23ce4a56918a671d254bf152b3333e
Yurii Khomin <yurii.khomin@gmail.com>
2021-04-30 21:33:35 +0300

    done

--- src/js/theme-mode.js
+++ src/js/theme-mode.js
@@ -11,13 +11,13 @@ const Theme = {
 refs.body.classList.add(Theme.LIGHT);
 refs.switcher.addEventListener('change', switchBox);
 
+const savedTheme = localStorage.getItem('Theme');
+
 function switchBox() {
   refs.body.classList.toggle(Theme.DARK);
 
   localStorage.setItem('Theme', refs.body.classList);
 };
-
-const savedTheme = localStorage.getItem('Theme');
 
 if (savedTheme === 'light-theme dark-theme') {
   refs.body.classList.add(Theme.DARK);
