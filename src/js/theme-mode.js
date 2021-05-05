a0ae2e3a306979ee00c4956bb546c3b1be1cfdab
Yurii Khomin <yurii.khomin@gmail.com>
2021-05-05 16:38:58 +0300

    theme class fixed

--- src/js/theme-mode.js
+++ src/js/theme-mode.js
@@ -3,28 +3,28 @@ const refs = {
   switcher: document.querySelector('.js-switch-input'),
 };
 
-const Theme = {
+const theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-refs.body.classList.add(Theme.LIGHT);
 refs.switcher.addEventListener('change', switchBox);
 
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  refs.body.classList.add(Theme.DARK);
-  refs.switcher.checked = true;
+if (localStorage.getItem('theme')) {
+  refs.body.classList.add(localStorage.getItem('theme'));
+  refs.switcher.checked = refs.body.classList.contains(theme.DARK)
+  ? true
+  : false;
 };
 
 function switchBox() {
-  if (refs.switcher.checked) {
-    refs.body.classList.add(Theme.DARK)
-    refs.body.classList.remove(Theme.LIGHT)
-    localStorage.setItem('theme', Theme.DARK)
+  if (this.checked) {
+    refs.body.classList.add(theme.DARK);
+    refs.body.classList.remove(theme.LIGHT);
+    localStorage.setItem('theme', theme.DARK);
   } else {
-    refs.body.classList.add(Theme.LIGHT)
-    refs.body.classList.remove(Theme.DARK)
-    localStorage.setItem('theme', Theme.LIGHT)
+    refs.body.classList.add(theme.LIGHT);
+    refs.body.classList.remove(theme.DARK);
+    localStorage.setItem('theme', theme.LIGHT);
   }
 };
